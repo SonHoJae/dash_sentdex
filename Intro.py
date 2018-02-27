799d60adb70b25d8033e08fd6f66d9b3877172e9
hojae <sonhj07@gmail.com>
2018-02-28 06:19:08 +0900

    Interactive User Interface

--- Intro.py
+++ Intro.py
@@ -1,22 +1,23 @@
 import dash
+from dash.dependencies import Input, Output
 import dash_core_components as dcc
 import dash_html_components as html
 
 app = dash.Dash()
 
 app.layout = html.Div(children=[
-    html.H1('Dash tutorial'),
-    dcc.Graph(id='example',
-              figure= {
-                'data':[
-                    {'x':[1,2,3,4,5], 'y':[5,6,7,2,1], 'type':'line', 'name' :'boats'},
-                    {'x':[1,2,3,4,5], 'y':[8,3,2,3,5], 'type':'bar','name' :'cars'}
-                ],
-                'layout':{
-                    'title':'Basic Dash Example'
-                }
-              })
+    dcc.Input(id='input', value='Enter something', type='text'),
+    html.Div(id='output')
     ])
+
+@app.callback(
+    Output(component_id='output', component_property='children'),
+    [Input(component_id='input',component_property='value')])
+def update_value(input_data):
+    try:
+        return str(float(input_data) ** 2)
+    except:
+        return "Some error"
 
 if __name__ == '__main__':
     app.run_server(debug=True)
